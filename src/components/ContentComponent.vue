76281425bb7048841521ae9f480f7515043ed932
Vladislav Smolkin <v.smolkin@infomaximum.com>
2023-09-09 17:04:46 +0300

    feat: made base visual [master]

---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="content">All sorts of content</main>
+</template>
+
+<style scoped lang="scss">
+@import 'src/assets/const.scss';
+.content {
+  background-color: $themeWhite;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  flex-grow: 1;
+  margin: 0 50px 58px 50px;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <div class="header__title">Жилфонд</div>
+    <div class="header__user-name">Пользователь</div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@import 'src/assets/const.scss';
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 46px 50px 30px 50px;
+}
+.header__title {
+  color: $themeRed;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 39px;
+}
+
+.header__user-name {
+  color: $themeBlack;
+  font-size: 16px;
+}
+</style>
